907d66b68813b22489f7ff1fa3d74cb914a20c29
mizne <w20054319@126.com>
2017-11-23 19:21:16 +0800

    fix package.json

--- src/lib/storage.config.ts
+++ src/lib/storage.config.ts
@@ -13,33 +13,21 @@ export class Action {
 }
 
 export class SetAction extends Action {
-  static create(key: string, value: any, expiredIn: string): SetAction {
-    return new SetAction(key, value, expiredIn)
-  }
   constructor(public key: string, public value: any, public expiredIn: string) {
     super()
   }
 }
 export class GetAction extends Action {
-  static create(key: string): GetAction {
-    return new GetAction(key)
-  }
   constructor(public key: string) {
     super()
   }
 }
 export class RemoveAction extends Action {
-  static create(key: string): RemoveAction {
-    return new RemoveAction(key)
-  }
   constructor(public key: string) {
     super()
   }
 }
 export class ClearAction extends Action {
-  static create(): ClearAction {
-    return new ClearAction()
-  }
   constructor() {
     super()
   }
